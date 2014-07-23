22f1fcae6a38a2b9a2492138076295776c741c38
Jakub Jeleński <jakub.jelenski@runbox.com>
2014-07-24 01:31:10 +0200

    Remove not supported python versions

--- setup.py
+++ setup.py
@@ -42,15 +42,9 @@ setup(
     keywords='SMPD',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements
