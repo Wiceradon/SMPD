62eda7ff3809d8e04e7a50d0ad4a2f055be3fdb2
Wiceradon <jakub.jelenski@runbox.com>
2014-07-26 00:58:52 +0200

    https://github.com/Wiceradon/SMPD/issues/1 - Generator initializer

--- SMPD/generators/__init__.py
+++ SMPD/generators/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Jakub Jeleński'
 __email__ = 'jakub.jelenski@runbox.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- SMPD/generators/points_generator.py
+++ SMPD/generators/points_generator.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 
-class generator(object):
+
+class Generator(object):
     '''
-    TODO:
+    Class used for generating list of N points in M dimennsions (M features).
     '''
+
+    ERRORS = {-1: "Feature size must be positive", -2: "Number of class must \
+be positive"}
+
+    def __init__(self, possible_coordinates):
+        '''
+        Simplest initializer it sets only number_of_classes.
+
+        :param possible_coordinates: 2D list of possible coordinate that point
+        can have. It cannot be None or empty.
+        '''
+        if not possible_coordinates:
+            raise ValueError("Construction param must not be empty")
+        for i in possible_coordinates:
+            if not i:
+                raise ValueError("Construction param cannot has empty\
+ sub-array")
+        self.number_of_classes = len(possible_coordinates)
+        self.default_class_size = None
+        self.number_of_features = None
+        self.possible_coordinates = possible_coordinates
