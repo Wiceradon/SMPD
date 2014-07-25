780bbce0eea1fb0eee56ecb145ed568d049ae236
Jakub Jeleński <jakub.jelenski@runbox.com>
2014-07-25 21:09:16 +0200

    Start development on https://github.com/Wiceradon/SMPD/issues/1

---
+++ SMPD/generators/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jakub Jeleński'
+__email__ = 'jakub.jelenski@runbox.com'
+__version__ = '0.1.0'
---
+++ SMPD/generators/points_generator.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class generator(object):
+    '''
+    TODO:
+    '''
