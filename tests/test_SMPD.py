6f1715ff79d2c7e04fc18170a54d26e5d5ef7064
Wiceradon <jakub.jelenski@runbox.com>
2014-07-29 21:40:10 +0200

    Put all tests in one file (for now at least)

--- tests/test_SMPD.py
+++ tests/test_SMPD.py
@@ -24,5 +24,17 @@ class TestSmpd(unittest.TestCase):
     def tearDown(self):
         pass
 
+class test_model(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
